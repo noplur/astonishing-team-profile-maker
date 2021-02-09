35e70f7ccb64a2a53e7af065cae4dc291faaf87f
noplur <geoduckphoto@gmail.com>
2021-02-08 18:14:48 -0800

    initial test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,10 @@
+const Employee = require('../lib/Employee.js');
+
+test('creates an employee object', () => {
+    // const employee = new Employee();
+  
+    // expect(employee.name).toEqual(expect.any(String));
+    // expect(employee.name.length).toBeGreaterThan(0);
+    // expect(employee.value).toEqual(expect.any(Number));
+  });
+  
