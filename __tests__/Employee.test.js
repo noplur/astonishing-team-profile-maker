bedeeca799b7b8f45de2290daf1a5704e53aa5af
noplur <geoduckphoto@gmail.com>
2021-02-10 22:58:07 -0800

    update to functions

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,5 +3,5 @@ const Employee = require('../lib/Employee.js');
 test('creates an Employee name object', () => {
     const employee = new Employee('EmployeeName');
   
-    expect(employee.EmployeeName).toBe('EmployeeName');
+    expect(employee.managerName).toBe('EmployeeName');
 });
